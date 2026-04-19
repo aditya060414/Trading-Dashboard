5202a040c8e26e21bf1568eecc6da01a7ba7c58c
aditya060414 <singh.aditya.44618@gmail.com>
2026-04-19 18:43:15 +0530

    better handling of authentication

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,34 +1,21 @@
 import { Navigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import axios from "axios";
 import { useAuth } from "./Auth";
 import { LoaderCircle } from "lucide-react";
+
 export default function ProtectedRoute({ children }) {
-  const [loading, setLoading] = useState(true);
-  const [auth, setAuth] = useState(false);
-  const { setUser } = useAuth();
+  const { user, loading } = useAuth();
 
-  useEffect(() => {
-    axios
-      .get("https://trading-backend-tf3j.onrender.com/api/v1/auth/verify", {
-        withCredentials: true,
-      })
-      .then((res) => {
-        setAuth(res.data.authenticated);
-        if (res.data.authenticated) {
-          setUser(res.data.user);
-        }
-        setLoading(false);
-      })
-      .catch(() => setLoading(false));
-  }, []);
-
-  if (loading)
+  if (loading) {
     return (
       <div className="load-circle">
         <LoaderCircle className="spinner" />
       </div>
     );
+  }
 
-  return auth ? children : <Navigate to="/login" replace />;
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
 }
