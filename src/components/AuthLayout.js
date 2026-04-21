ccb51965be97f816f0480519022e10ef6f1e7e0a
aditya060414 <singh.aditya.44618@gmail.com>
2026-04-21 17:20:48 +0530

    theme

--- src/components/AuthLayout.js
+++ src/components/AuthLayout.js
@@ -1,16 +1,45 @@
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SignUp from "./SignUp";
 import Login from "./Login";
 import Button from "@mui/material/Button";
+import { Sun, Moon } from 'lucide-react';
 
 export default function AuthLayout() {
   const [isSignUp, setIsSignUp] = useState(false);
+  const [dark, setDark] = useState(false);
+
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+
+    if (theme === "dark") {
+      setDark(true);
+      document.body.classList.add("dark");
+    } else {
+      setDark(false);
+      document.body.classList.remove("dark");
+    }
+  }, []);
+
+  const handleTheme = () => {
+    const newTheme = !dark;
+
+    setDark(newTheme);
+
+    if (newTheme) {
+      document.body.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.body.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  };
   const handleButtonClick = () => {
     setIsSignUp(!isSignUp);
   };
   return (
     <>
+      <button onClick={handleTheme} className="theme-button">{!dark ? <Sun className="lightheme" /> : <Moon className="darktheme" />}</button>
       <div className="auth-layout">
         <div className="auth-layout-container">
           <div className="authbrandf-info">
@@ -25,15 +54,15 @@ export default function AuthLayout() {
               </p>
             </div>
             <div className="graph-img">
-              <img src="media/graph.png" alt="graph-image" />
+              <img src={`media/${!dark ? "graph" : "darkGraph"}.png`} alt="graph-image" />
             </div>
           </div>
           <div className="signup-login toggle">
-            {isSignUp ? <SignUp /> : <Login />}
+            {isSignUp ? <SignUp theme={dark} /> : <Login theme={dark} />}
             <Button variant="contained" onClick={handleButtonClick} className="toggle-Btn">
               {isSignUp ? "Login" : "SignUp"}
             </Button>
-            {isSignUp ? <p style={{"display":"flex","position":"relative","justifyContent":"flex-end","right":"60px"}} className="toggle-text">Already have an account?</p> : <p style={{"display":"flex","position":"relative","justifyContent":"flex-end","right":"60px"}} className="toggle-text">Forgot Password?</p>}
+            {isSignUp ? <p style={{ "display": "flex", "position": "relative", "justifyContent": "flex-end", "right": "60px" }} className="toggle-text">Already have an account?</p> : <p style={{ "display": "flex", "position": "relative", "justifyContent": "flex-end", "right": "60px" }} className="toggle-text">Forgot Password?</p>}
           </div>
         </div>
       </div>
