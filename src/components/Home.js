d42a3bc9d1168416c78294af57e2f49f8e0050ca
aditya060414 <singh.aditya.44618@gmail.com>
2026-04-21 22:48:43 +0530

    dark and light theme implementation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,39 @@
 import TopBar from "./TopBar";
 import DashBoard from "./Dashboard";
-
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [dark, setDark] = useState(false);
+
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+
+    if (theme === "dark") {
+      setDark(true);
+      document.body.classList.add("dark");
+    } else {
+      setDark(false);
+      document.body.classList.remove("dark");
+    }
+  }, []);
+
+  const handleTheme = () => {
+    const newTheme = !dark;
+
+    setDark(newTheme);
+
+    if (newTheme) {
+      document.body.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.body.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  };
   return (
     <>
       <div className="home-container">
-        <TopBar />
+        <TopBar dark={dark} handleTheme={handleTheme} />
         <DashBoard />
       </div>
     </>
