f52f09dd07981a4e8f8c7fc8fdf85ed83ea77739
aditya060414 <singh.aditya.44618@gmail.com>
2026-04-26 17:38:55 +0530

    create username div

---
+++ src/components/changeUsername.js
@@ -0,0 +1,12 @@
+import {useState} from 'react';
+
+export default function ChangeUsername() {
+  return (
+    <div className='change-wrappet'>
+        <div className="Account-page">
+            <h2><ul className='change-heading'><li>CHANGE USERNAME</li></ul></h2>
+            
+        </div>
+    </div>
+  )
+}
